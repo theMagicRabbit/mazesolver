bc221daae6d79c0d4c2dfde0b6bd19a3ae29e0da
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-04-24 21:54:17 -0500

    Add cell class

--- mazesolver.py
+++ mazesolver.py
@@ -14,10 +14,42 @@ class Line():
         self.p2 = p2
 
     def draw(self, canvas: Canvas, line_color: str):
-        canvas.create_line((self.p1.x, self.p1.y), (self.p2.x, self.p2.y), fill=line_color, width=2)
+        canvas.create_line((self.p1.x, self.p1.y), (self.p2.x, self.p2.y), fill=line_color, width=16)
 
     def __repr__(self):
         return f"Line({self.p1}, {self.p2})"
+
+class Cell():
+    def __init__(self, x1, y1, x2, y2, window, top_side=True, right_side=True, bottom_side=True, left_side=True):
+        self.has_top = top_side
+        self.has_right = right_side
+        self.has_bottom = bottom_side
+        self.has_left = left_side
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self.top_left = Point(x1, y1)
+        self.top_right = Point(x2, y1)
+        self.bottom_left = Point(x1, y2)
+        self.bottom_right = Point(x2, y2)
+        self._window = window
+
+    def draw(self):
+        sides = []
+        if self.has_top:
+            sides.append(Line(self.top_left, self.top_right))
+        if self.has_left:
+            sides.append(Line(self.top_left, self.bottom_left))
+        if self.has_right:
+            sides.append(Line(self.top_right, self.bottom_right))
+        if self.has_bottom:
+            sides.append(Line(self.bottom_left, self.bottom_right))
+        for s in sides:
+            self._window.draw_line(s, "green")
+
+    def __repr__(self):
+        return f"Cell({self._x1}, {self._y1}, {self._x2}, {self._y2}, {self.has_top}, {self.has_right}, {self.has_bottom}, {self.has_left})"
 
 class Window():
     def __init__(self, width: int, height: int):
@@ -43,24 +75,26 @@ class Window():
         self.is_running = False
 
     def draw_line(self, line: Line, fill_color: str):
+        print(line)
         line.draw(self.canvas, fill_color)
     def __repr__(self):
         return f"Window({self.width}, {self.height})"
 
 def main():
     win = Window(800, 600)
-    p1 = Point(10, 100)
-    p2 = Point(10, 10)
-    p3 = Point(0, 0)
-    p4 = Point(800, 600)
-    p5 = Point(100, 700)
-    p6 = Point(17, 90)
-    l1 = Line(p1, p2)
-    l2 = Line(p3, p4)
-    l3 = Line(p5, p6)
-    win.draw_line(l1, "red")
-    win.draw_line(l2, "green")
-    win.draw_line(l3, "blue")
+    cells = [
+        Cell(110, 110, 111, 111, win, True, True, True, True),
+        Cell(700, 500, 701, 501, win, True, True, True, False),
+        Cell(100, 101, 101, 102, win, True, True, False, True),
+        Cell(200, 200, 201, 201, win, True, False, True, True),
+        Cell(300, 300, 301, 301, win, False, True, True, True),
+        Cell(400, 400, 401, 401, win, True, True, False, False),
+        Cell(500, 500, 501, 501, win, True, False, False, False),
+        Cell(600, 510, 601, 511, win, True, False, True, False),
+        Cell(650, 510, 651, 511, win, False, False, False, False),
+    ]
+    for c in cells:
+        c.draw()
     win.wait_for_close()
 
 if __name__ == '__main__':
