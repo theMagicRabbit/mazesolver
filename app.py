e41adc1c8f7cd3db594b2f807a06f2215c90ace6
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-05-10 13:04:33 -0500

    Match cols and rows to x and y

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ def main():
     x = 802
     y = 602
     cell_size = 25
-    rows = floor(800 / cell_size)
-    cols = floor(600 / cell_size)
+    cols = floor(800 / cell_size)
+    rows = floor(600 / cell_size)
     win = Window(x, y)
-    maze = Maze(1, 1, rows, cols, cell_size, cell_size, win)
+    maze = Maze(1, 1, cols, rows, cell_size, cell_size, win)
     win.wait_for_close()
 
 if __name__ == '__main__':
